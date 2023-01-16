c75ce0fd68d8cf797c2fc1de23ad442e3b7aa06a
SamuelArico99 <samuel16699.sa@gmail.com>
2023-01-16 13:04:02 +0100

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// Step:
+
+// 1. Creare l'array di oggetti con le informazioni fornite.
+
+// 2. Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// 3. Stampare le stesse informazioni su DOM sottoforma di stringhe
