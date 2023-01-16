9d99f7e428f73c3a94cf8251d121bba694d48692
SamuelArico99 <samuel16699.sa@gmail.com>
2023-01-16 16:48:18 +0100

    creato array di oggetti e stampato su console

--- script.js
+++ script.js
@@ -5,6 +5,47 @@
 
 // 1. Creare l'array di oggetti con le informazioni fornite.
 
+const members = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chef Editor',
+        foto: 'Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'Walter Gordon	Office Manager	walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'Scott Estrada	Developer	scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg',
+    }
+];
+
 // 2. Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+
+};
+const membersContainer = document.getElementById('members-container');
+membersContainer.innerHTML = members;
 // 3. Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
